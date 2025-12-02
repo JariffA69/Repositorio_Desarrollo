6fc7eb09639d788f8583eac635b2bf969b279538
JariffA69 <2402064@utrivieramaya.edu.mx>
2025-12-01 22:30:13 -0500

    Modificación completa de home page, se anexan nuevos files en limpio en modo stand by para dejar un buen diseño a futuro

--- desarrollo-web/login-home/home2.ts
+++ desarrollo-web/login-home/home2.ts
@@ -1,12 +1,13 @@
-import './home.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// @ts-ignore: allow importing CSS files without type declarations
+import './home2.css';
 import '../../AppTypes';
 
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log('Home page loaded');  
 
-  const backBtn = document.getElementById('backBtn');
+  const backBtn = document.getElementById('btn_InicioSesion');
   if (backBtn) {
     backBtn.addEventListener('click', () => {
     window.appNav.toLogin()
